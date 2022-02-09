968a283d95ef13f2f5e93df29f5a3822583b06f2
Serch00 <serchland.net@outlook.com>
2022-02-09 21:43:09 +0100

    Step 2

---
+++ Modules/PrismWPFHoneys.Modules.Mail/MailModule.cs
@@ -0,0 +1,20 @@
+﻿using PrismWPFHoneys.Modules.Mail.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace PrismWPFHoneys.Modules.Mail
+{
+    public class MailModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+ 
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
