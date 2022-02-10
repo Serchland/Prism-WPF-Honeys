95ad54a6f996711116eecbf2a4b8ea3308a0a162
Serch00 <serchland.net@outlook.com>
2022-02-10 19:38:28 +0100

    Step 3

--- Modules/PrismWPFHoneys.Modules.Mail/MailModule.cs
+++ Modules/PrismWPFHoneys.Modules.Mail/MailModule.cs
@@ -2,14 +2,21 @@
 using Prism.Ioc;
 using Prism.Modularity;
 using Prism.Regions;
+using PrismWPFHoneys.Core;
 
 namespace PrismWPFHoneys.Modules.Mail
 {
     public class MailModule : IModule
     {
+        IRegionManager _regionManager;
+
+        public MailModule(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
         public void OnInitialized(IContainerProvider containerProvider)
         {
- 
+            _regionManager.RegisterViewWithRegion(RegionNames.ContentRegion, typeof(ViewA));
         }
 
         public void RegisterTypes(IContainerRegistry containerRegistry)
---
+++ PrismWPFHoneys.Core/RegionNames.cs
@@ -0,0 +1,7 @@
+﻿namespace PrismWPFHoneys.Core
+{
+    public static class RegionNames
+    {
+        public const string ContentRegion = "ContentRegion";
+    }
+}
