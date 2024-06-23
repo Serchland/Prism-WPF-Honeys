caa749e64a08c1b49193da95dbab84cee458b84d
Serch00 <serchland.net@outlook.com>
2024-06-23 13:50:34 +0200

    login

--- Prism WPF Honeys/ViewModels/LoginViewModel.cs
+++ Prism WPF Honeys/ViewModels/LoginViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using Prism.Commands;
 using Prism.Events;
 using PrismWPFHoneys.Core.Types.Events;
+using PrismWPFHoneys.Core.Types.Interfaces;
 using PrismWPFHoneys.Core.Types.Types;
+using PrismWPFHoneys.Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,9 +15,11 @@ namespace Prism_WPF_Honeys.ViewModels
 {
     public class LoginViewModel
     {
+        private IGestDocService _gestDocService;
         private IEventAggregator _ea;
-        public LoginViewModel(IEventAggregator ea)
+        public LoginViewModel(IEventAggregator ea, IGestDocService GestDocService)
         {
+            _gestDocService = GestDocService;
             _ea = ea;
             //TODO: Do login tasks
         }
@@ -55,6 +59,7 @@ namespace Prism_WPF_Honeys.ViewModels
         private void ThreadGestDoc()
         {
             Thread.Sleep(0);
+            //_gestDocService.GetBoletines();
             _ea.GetEvent<OnModuleInitializedEvent>().Publish(AppModuleNames.GestDocModuleName);
         }
     }
